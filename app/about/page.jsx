bdd97611f78dc012a8d9c062d116b34422f349af
linuxguy467 <me@matthem.codes>
2023-05-01 08:20:56 -0400

    add about page and team page; cleanup

---
+++ app/about/page.jsx
@@ -0,0 +1,18 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque eu
+        tincidunt tortor aliquam nulla. At varius vel pharetra vel turpis nunc eget lorem. Quisque sagittis purus sit amet volutpat consequat mauris
+        nunc. Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Sed risus pretium quam vulputate. Senectus et netus et malesuada
+        fames ac turpis. Scelerisque eu ultrices vitae auctor eu augue. Ultrices mi tempus imperdiet nulla. In nibh mauris cursus mattis molestie a
+        iaculis at. Non enim praesent elementum facilisis leo vel. At elementum eu facilisis sed odio morbi quis commodo. Fringilla est ullamcorper
+        eget nulla facilisi etiam dignissim. Nec feugiat in fermentum posuere. Et malesuada fames ac turpis. Augue interdum velit euismod in
+        pellentesque. Mi sit amet mauris commodo quis imperdiet massa. Vivamus arcu felis bibendum ut.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ app/about/team/page.jsx
@@ -0,0 +1,13 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nemo aliquam totam. Error, maiores atque ipsa neque omnis ab eius iste, tempora
+        alias sequi non quibusdam obcaecati perspiciatis quis et.
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
